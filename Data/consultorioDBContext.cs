8a6370e9a70a194354a82a22a841cbc4653608e7
Gustavo Penagos <110733999+GustavoPenagos@users.noreply.github.com>
2023-07-15 11:37:33 -0500

    Creacion de data

---
+++ Data/consultorioDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdontologiaWeb.Models;
+
+namespace ConsultorioAPI.Data
+{
+    public class consultorioDBContext : DbContext
+    {
+        public consultorioDBContext(DbContextOptions<consultorioDBContext>options):base (options) { }
+
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Anamnesis> Anamnesis { get; set;}
+        public DbSet<Ant_Familiar> Ant_Familiar { get; set; }
+        public DbSet<TipoDocumento> TipoDocumento { get; set;}
+        public DbSet<Ciudad> Ciudad { get; set; }
+        public DbSet<Departamento> Departamento { get; set;}
+        public DbSet<Estomatologico> Estomatologico { get; set;}
+        public DbSet<Genero> Genero { get; set; }
+
+    }
+}

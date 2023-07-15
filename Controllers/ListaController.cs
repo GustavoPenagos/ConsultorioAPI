9297e798816c5933271932fda740481ada139b28
Gustavo Penagos <110733999+GustavoPenagos@users.noreply.github.com>
2023-07-15 11:20:18 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/ListaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace ConsultorioAPI.Controllers
+{    
+    [ApiController]
+    public class ListaController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+        public ListaController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("/api/lista/pacientes")]
+        public dynamic ListaPacientes()
+        {
+            var con = Configuration.GetConnectionString("OdontologiaDB");
+            try
+            {
+                string query = "SELECT * FROM Usuario";
+                using (SqlConnection connection = new SqlConnection(con))
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                    DataTable data = new DataTable();
+                    adapter.Fill(data);
+                    return  JsonConvert.SerializeObject(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Controllers/RegistroController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using OdontologiaWeb.Models;
+
+namespace ConsultorioAPI.Controllers
+{
+    [ApiController]
+    public class RegistroController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+        public RegistroController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        [HttpPost]
+        [Route("/registro/usuario")]
+        public dynamic RegistroUsuario(Usuario usuario)
+        {
+            try
+            {
+                string query = "INSERT INTO [dbo].[Usuario]  VALUES ()";
+                var con = Configuration.GetConnectionString("OdontologiaDB");
+                using(SqlCommand cmd = new SqlCommand(con))
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return "";
+        }
+        [HttpPost]
+        [Route("/registro/anamnesis")]
+        public dynamic RegistroAnamnesis(Anamnesis anamnesis)
+        {
+            return "";
+        }
+        [HttpPost]
+        [Route("/registro/familiar")]
+        public dynamic RegistroFamiliar(Ant_Familiar familiar)
+        {
+            return "";
+        }
+        [HttpPost]
+        [Route("/registro/estomatologico")]
+        public dynamic RegistroEstomatologico(Estomatologico estomatologico)
+        {
+            return "";
+        }
+    }
+}
